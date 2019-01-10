a9632b3a4aebdc21aa741276b1b0f178eb0a2935
jademh <jademh@gmail.com>
2019-01-10 14:44:40 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.fetchData = this.fetchData.bind(this);
+    this.pickRandomEp = this.pickRandomEp.bind(this);
+  }
+
+  state = {
+    showId: 4586,
+    //showId: 105,
+    loaded: false,
+    seasons: [],
+    seasonCount: 0,
+    episodeCount: 0,
+    randomEpisodeDetails: null,
+  }
+
+  fetchData() {
+    const fetchPath = `https://api.themoviedb.org/3/tv/${this.state.showId}?api_key=${API_KEY}&language=en-US`;
+    fetch(fetchPath)
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        seasons: data.seasons,
+        seasonCount: data.seasons.length,
+        episodeCount: data.number_of_episodes,
+      }, this.pickRandomEp);
+    });
+  }
+
+  pickRandomEp() {
+    const randomSeason = Math.floor(Math.random() * (this.state.seasonCount));
+    const epsInRandomSeason = this.state.seasons[randomSeason].episode_count;
+    const randomEpisode = Math.floor(Math.random() * epsInRandomSeason) + 1;
+    const fetchPath = `https://api.themoviedb.org/3/tv/${this.state.showId}/season/${randomSeason}/episode/${randomEpisode}?api_key=${API_KEY}&language=en-US`;
+    fetch(fetchPath)
+    .then(response => response.json())
+    .then(data => {
+      this.setState({ randomEpisodeDetails: data, loaded: true});
+    });
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    if(this.state.loaded) {
+      return (
+        <div className="App">
+          <div>
+            <span>Season {this.state.randomEpisodeDetails.season_number}</span>
+            <span> Episode {this.state.randomEpisodeDetails.episode_number}</span>
+          </div>
+          <div>{this.state.randomEpisodeDetails.name}</div>
+          <div>{this.state.randomEpisodeDetails.overview}</div>
+          <button onClick={this.pickRandomEp}>Give me another</button>
+        </div>
+      );
+    }
+    return(
+      <div>... nothing yet</div>
+    )
   }
 }
 
