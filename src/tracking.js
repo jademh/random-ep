9000aff29b493f1badc802f76c1d629ae8b48fb1
jademh <jademh@gmail.com>
2019-02-13 14:35:20 +0000

    add google analytics and event tracking

--- src/tracking.js
+++ src/tracking.js
@@ -1,6 +1,11 @@
+/* global gtag */
+
 export default function trackEvent(eventCategory, eventAction, eventLabel) {
   if (process.env.NODE_ENV === 'production') {
-    //ga('send', 'event', eventCategory, eventAction, eventLabel);
+    gtag('event', eventAction, {
+      event_category: eventCategory,
+      event_label: eventLabel,
+    });
   } else {
     let consolePrefix = '👁👁 ';
     let consoleStyle =
