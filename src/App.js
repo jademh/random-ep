412705f3311af9ec6b8ec2b8fd1425929eef5c17
jademh <jademh@gmail.com>
2019-02-13 15:47:19 +0000

    add custom property for viewport height for iOS

--- src/App.js
+++ src/App.js
@@ -84,6 +84,20 @@ export default function App() {
     }
   }, [showChosen]);
 
+  const updateVh = () => {
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', updateVh);
+    return () => {
+      window.removeEventListener('resize', updateVh);
+    };
+  }, []);
+
   if (error !== '') {
     return (
       <div className="loading">
